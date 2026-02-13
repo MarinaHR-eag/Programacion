754e3018cf21a1f2ffd51ce0abbd22bf0f5b7205
MarinaHR-eag <marina.hurtado@escuelaartegranada.com>
2026-02-13 19:52:27 +0100

    cambios añadidos a Departamento
    Se han añadido los cambios necesarios para hacer que la clase Departamento sea robusta y coherente con el diagrama, evitando que haya estados erróneos

--- SistemaEmpleados/src/sistemaempleados/Departamento.java
+++ SistemaEmpleados/src/sistemaempleados/Departamento.java
@@ -33,7 +33,7 @@ public class Departamento {
         Empleado[] empleados){
         this.nombre = nombre;
         this.presupuestoAnual = presupuesto;
-        this.empleados = empleados;
+        this.empleados = empleados.clone();
         // Constrolamos que el director, si se pasa, está dentro del array de empleados
         this.setDirector(director);
     }
@@ -43,7 +43,7 @@ public class Departamento {
         this.nombre = d.nombre;
         this.presupuestoAnual = d.presupuestoAnual;
         this.director = d.director;
-        this.empleados = d.empleados;
+        this.empleados = d.empleados.clone();
     }
     
     // Getters
@@ -60,7 +60,7 @@ public class Departamento {
     }
     
     public Empleado[] getEmpleados(){
-        return this.empleados;
+        return this.empleados.clone();
     }
     
     // Setters
@@ -73,41 +73,84 @@ public class Departamento {
     }
     
     public void setDirector(Empleado director){
-        // Comprobar si el director está en el departamento
-        boolean directorEnListaDeEmpleados = false;
+        // Si hemos pasado un empleado para ser director, comprobamos si ya
+        // pertenece al departamento, y en el caso de que no, lo añadimos a la
+        // lista de empleados del departamento
         if(director != null){
-            directorEnListaDeEmpleados = comprobarDirectorPerteneceADepartamento(director);
-        }
-        if(!directorEnListaDeEmpleados){
-            this.aniadirNuevoEmpleado(director);
+            // Comprobar si el director está en el departamento
+            boolean directorEnListaDeEmpleados = comprobarEmpleadoPerteneceADepartamento(director);
+            if(!directorEnListaDeEmpleados){
+                this.aniadirNuevoEmpleado(director);
+            }
         }
         this.director = director;
     }
     
     public void setEmpleados(Empleado[] empleados){
-        this.empleados = empleados;
+        this.empleados = empleados.clone();
+        // Además de añadir los empleados, cambiamos su departamento por mantener la consistencia
+        for(int i = 0; i < empleados.length; i++){
+            empleados[i].setDepartamento(this);
+        }
     }
     
     // Métodos de apoyo
-    private boolean comprobarDirectorPerteneceADepartamento(Empleado director){
-        boolean directorEnDepartamento = false;
+    private boolean comprobarEmpleadoPerteneceADepartamento(Empleado empleado){
+        boolean empleadoEnDepartamento = false;
         for(int i = 0; i < this.empleados.length; i++){
-            if(this.empleados[i] == director){
-                directorEnDepartamento = true;
+            if(this.empleados[i] == empleado){
+                empleadoEnDepartamento = true;
                 break;
             }
         }
         
-        return directorEnDepartamento;
+        return empleadoEnDepartamento;
     }
     
     private void aniadirNuevoEmpleado(Empleado nuevoEmpleado){  
-        Empleado[] nuevoArray = new Empleado[this.empleados.length + 1];
-        for(int i = 0; i < this.empleados.length; i++){
-            nuevoArray[i] = this.empleados[i];
+        // Comprobamos que no nos han pasado un valor nulo
+        if(nuevoEmpleado != null){
+            // Comprobamos que el empleado no está ya en el departamento
+            boolean empleadoYaAniadido = comprobarEmpleadoPerteneceADepartamento(nuevoEmpleado);
+            if(!empleadoYaAniadido){
+                // Comprobamos que el empleado no estaba antes en otro departamento
+                // Y en caso de que sí lo estuviese, lo borramos de la lista de 
+                // empleados de ese departamento
+                Departamento departamento = nuevoEmpleado.getDepartamento();
+                if(departamento != null && departamento != this){
+                    departamento.eliminarEmpleado(nuevoEmpleado);
+                }
+                Empleado[] nuevoArray = new Empleado[this.empleados.length + 1];
+                for(int i = 0; i < this.empleados.length; i++){
+                    nuevoArray[i] = this.empleados[i];
+                }
+                nuevoArray[nuevoArray.length - 1] = nuevoEmpleado;
+                this.empleados = nuevoArray;
+                nuevoEmpleado.setDepartamento(this);
+            } 
         }
-        nuevoArray[nuevoArray.length - 1] = nuevoEmpleado;
-        this.empleados = nuevoArray;
+    }
+    
+    // Creamos un método para eliminar un empleado de un departamento en el caso de
+    // que este cambie de departamento o haya que borrarlo definitivamente
+    private void eliminarEmpleado(Empleado empleadoAEliminar){
+        // Inicialmente comprobamos si el empleado que queremos eliminar pertenece al departamento
+        boolean empleadoPertenece = comprobarEmpleadoPerteneceADepartamento(empleadoAEliminar);
+        if(empleadoPertenece){
+            // Creamos un array auxiliar de empleados con 1 empleado menos que el que tenemos
+            Empleado[] nuevoArray = new Empleado[this.empleados.length - 1];
+            // Creamos un iterador para ir añadiendo empleados en el nuevo array
+            int iterador = 0;
+            for(int i = 0; i < this.empleados.length; i++){
+                // En el caso de que el empleado que estamos recorriendo sea
+                // distinto al que queremos eliminar, lo añadimos al array auxiliar
+                if(this.empleados[i] != empleadoAEliminar){
+                    nuevoArray[iterador] = this.empleados[i];
+                    iterador++;
+                }
+            }
+            this.empleados = nuevoArray;
+        }
     }
     
     // Métodos personalizados
